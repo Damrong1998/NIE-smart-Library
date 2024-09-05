52711f814b174de361f8e4b0f07f54684975c3fb
Damrong1998 <hol.damrong@gmail.com>
2024-09-05 11:54:00 +0700

    e

--- app/admin/students/edit/[id]/page.js
+++ app/admin/students/edit/[id]/page.js
@@ -4,6 +4,8 @@ import EditFormStudent from '../../forms/EditFormStudent';
 import { getData, getDataById } from '@/app/db/function/CRUD';
 import { Typography } from '@mui/material';
 
+export const dynamic = 'force-static'
+
 function EditStudent({params}) {
     const {id} = params;
 
