14635734fd9b84d08ae8dd931843a1730be52c02
Damrong1998 <hol.damrong@gmail.com>
2024-09-05 12:04:06 +0700

    e

--- app/admin/students/edit/[id]/page.js
+++ app/admin/students/edit/[id]/page.js
@@ -1,5 +1,5 @@
 // "use client"
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import EditFormStudent from '../../forms/EditFormStudent';
 import { getData, getDataById } from '@/app/db/function/CRUD';
 import { Typography } from '@mui/material';
